1ee1d2c287a2bc4f6da0201cd4a154c84e48e4ee
askfiy <c2323182108@gmail.com>
2022-07-27 19:30:22 +0800

    First commit

---
+++ clear_adapter_table_20220727.sql
@@ -0,0 +1,26 @@
+﻿DELETE FROM layer4_1_om.adapter_order
+WHERE order_id in
+    (
+	SELECT order_id
+	FROM layer4_1_om.adapter_order
+	WHERE created_timestamp < now() - '2 day'::interval
+	LIMIT 10000
+    );
+
+DELETE FROM layer4_1_om.adapter_idemporent
+WHERE req_code in
+    (
+	SELECT req_code
+	FROM layer4_1_om.adapter_idemporent
+	WHERE created_timestamp < now() - '2 day'::interval
+	LIMIT 10000
+    );
+
+DELETE FROM layer4_1_om.adapter_message
+WHERE id in
+    (
+	SELECT id
+	FROM layer4_1_om.adapter_message
+	WHERE created_timestamp < now() - '2 day'::interval
+	LIMIT 10000
+    );
